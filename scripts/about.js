3656135dc0401311d06dd1d62afd709fcba0b14f
cale-pliska <59024810+cale-pliska@users.noreply.github.com>
2021-11-26 16:06:49 -0600

    improving expandable sections

--- scripts/about.js
+++ scripts/about.js
@@ -1,9 +1,15 @@
-$(".title-container").click(function () {
+let readMore = document.createTextNode(". . .");
+let $teaser = $(".content-teaser")
 
-    $header = $(this);
+$teaser.append(readMore);
+
+$teaser.click(function () {
+
+    $teaser = $(this);
     //getting the next element
-    $content = $header.next();
+    $content = $teaser.next();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+
     $content.slideToggle(500);
 
 });
